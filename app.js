f0137f16b828aee5a0649100d52a89b2f91f4c1c
mat0015 <mat0015@hotmail.com>
2021-09-19 10:38:58 -0300

    Subiendo los archivos

---
+++ app.js
@@ -0,0 +1,4 @@
+    let claseApellido = prompt("Ingrese su apellido")
+    let claseNombre = prompt("Ingrese su nombre")
+    let alumno = "El alumno se llama " + claseApellido + " " + claseNombre
+    alert (alumno)
