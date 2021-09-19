ec15c17f61ec3b5f6550a9dbe3daa3f26e5bbaa2
mat0015 <91016110+mat0015@users.noreply.github.com>
2021-09-19 11:40:01 -0300

    Resubiendo archivo app

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
     let claseApellido = prompt("Ingrese su apellido")
     let claseNombre = prompt("Ingrese su nombre")
-    let alumno = "El alumno se llama " + claseApellido + " " + claseNombre
+    let alumno = "Hola " + claseApellido + " " +claseNombre
     alert (alumno)
+    let numero1 = prompt("Ingrese un número")
+    let numero2 = 2.3
+    let suma = ("La suma de ambos números es de " + ((parseInt(numero1))  + numero2))
+    console.log (typeof numero1)
+    console.log (typeof numero2)
+    alert (suma)
+
+  
